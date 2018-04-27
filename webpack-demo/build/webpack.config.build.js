f2c314a46958e92d3d4aa9850d9c524a2590737b
ruohua <54871181@qq.com>
2018-04-27 09:14:36 +0800

    webapack-demo + 说明

---
+++ webpack-demo/build/webpack.config.build.js
@@ -0,0 +1,14 @@
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+	devtool: "eval-source-map",
+	context: path.resolve(__dirname, "../"),
+	entry: {
+		"/js/main": "./src/js/main.js" //入口文件
+	},
+	output: {
+		path: path.resolve(__dirname, "../dist"), //打包后的文件存放的地方
+		filename: "[name].build.js" //打包后输出文件的文件名
+	},
+}
---
+++ webpack-demo/build/webpack.config.dev.js
@@ -0,0 +1,26 @@
+const webpack = require("webpack");
+const path = require("path");
+
+
+module.exports = {
+	devtool: "eval-source-map",
+	context: path.resolve(__dirname, "../"), //基础目录
+	entry: {
+		//"/js/main": "./src/js/main.js" //入口文件
+		main: "./src/js/main.js" //入口文件
+	},
+	output: {
+		path: path.resolve(__dirname, "../dist"), //打包后的文件存放的地方
+		publicPath: "assets/", //虚拟路径，
+		filename: "[name].build.js" //打包后输出文件的文件名,[name]可以是路径/js/main
+	},
+	devServer: {
+		host: "192.168.0.95", //主机名
+		port: 9000, //端口号
+		contentBase: "./", //本地服务器所加载的页面所在的目录
+		//openPage: "index.html", //首页
+		historyApiFallback: true, //不跳转
+		inline: true //实时刷新
+		//hot: true	//热加载
+	}
+}
